fd3f4b7ddaf951bf32ea34c049223167d44c6361
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-01-23 11:34:15 +0530

    Assignment completed

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
-// API: Retrieve Students Above Threshold
-// ---------------------------------------
-// Task:
-// Implement an API to fetch students whose total marks exceed a given threshold.
-//
-// Endpoint:
-// POST /students/above-threshold
-//
-// Request Body:
-// {
-//   "threshold": <number>
-// }
-//
-// Response:
-// Success: List of students with their names and total marks who meet the criteria.
-// Example:
-// {
-//   "count": 2,
-//   "students": [
-//     { "name": "Alice Johnson", "total": 433 },
-//     { "name": "Bob Smith", "total": 410 }
-//   ]
-// }
-//
-// No Matches:
-// {
-//   "count": 0,
-//   "students": []
-// }
-//
-// Purpose:
-// Help teachers retrieve and analyze student performance efficiently.
-
-
 const express = require('express');
-const { resolve } = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
-const app = express();
-const port = 3010;
+const app= express();
+const PORT = 3000;
 
-app.use(express.static('static'));
+app.use(bodyParser.json()); 
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const studentDataPath = path.join(__dirname, 'data.json');
+const students = JSON.parse(fs.readFileSync(studentDataPath, 'utf8'));
+
+
+app.use('/students/above-threshold', (req,res)=>{
+    const {threshold} = req.body;
+
+
+    if(threshold <0){
+        return res.status(400).json({error: 'Invalid threshold value'});
+        //here is the condition if threshold is less than 0 error will display.   
+
+    }
+
+    const filteredStudents = students.filter((student)=>student.total>threshold);
+    //Filters the students array to include only those students whose total marks exceed the specified threshold
+    const response = {
+        count: filteredStudents.length,
+        students: filteredStudents.map(({name, total })=> ({
+            name, total
+        }))
+        //Uses destructuring to extract the name and total properties from each student object.
+        // Creates a new object for each students with only those properties
+        
+    };
+    return res.status(200).json(response);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
